25a5b54d20e2453cf2ee760a2327243fc09bc5d0
dahmanhamza-jpg <dahman.hamza@gmail.com>
2026-09-25 18:35:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ import {
   exportAll,
   importAll,
   syncRemote,
-  startAutoSync,
-  getSyncCode,
-  setSyncCode
+ startAutoSync,
+getSyncCode,
+setSyncCode
 } from './services/sync';
 import { downloadText, scriptText } from './lib/exportScript';
 import { BottomNav } from './components/BottomNav';
