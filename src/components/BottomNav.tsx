10d5c756cfabd7b89a419b584c13bcd026e23713
dahmanhamza-jpg <dahman.hamza@gmail.com>
2026-09-25 23:53:13 +0200

    Add Focus to primary navigation

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,3 +1,11 @@
 import { NavLink } from 'react-router-dom';
-import { Home, Users, CalendarDays, MoreHorizontal } from 'lucide-react';
-export function BottomNav(){ return <nav className="bottom-nav"><NavLink to="/"><Home/><span>Oggi</span></NavLink><NavLink to="/clienti"><Users/><span>Clienti</span></NavLink><div className="nav-gap"/><NavLink to="/calendario"><CalendarDays/><span>Calendario</span></NavLink><NavLink to="/altro"><MoreHorizontal/><span>Altro</span></NavLink></nav> }
+import { Home, Users, CalendarDays, MoreHorizontal, TimerReset } from 'lucide-react';
+export function BottomNav(){
+  return <nav className="bottom-nav">
+    <NavLink to="/"><Home/><span>Oggi</span></NavLink>
+    <NavLink to="/clienti"><Users/><span>Clienti</span></NavLink>
+    <NavLink to="/focus"><TimerReset/><span>Focus</span></NavLink>
+    <NavLink to="/calendario"><CalendarDays/><span>Calendario</span></NavLink>
+    <NavLink to="/altro"><MoreHorizontal/><span>Altro</span></NavLink>
+  </nav>
+}
