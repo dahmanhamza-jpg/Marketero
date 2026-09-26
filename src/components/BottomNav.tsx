0c2fdeb4dcda4f96da9cb14316efdf84c7afe637
dahmanhamza-jpg <dahman.hamza@gmail.com>
2026-09-26 02:40:02 +0200

    Scroll to top when switching bottom navigation sections

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,11 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import { Home, Users, CalendarDays, MoreHorizontal, TimerReset } from 'lucide-react';
+
 export function BottomNav(){
+  const goTop=()=>window.scrollTo({top:0,left:0,behavior:'auto'});
+
   return <nav className="bottom-nav">
-    <NavLink to="/"><Home/><span>Oggi</span></NavLink>
-    <NavLink to="/clienti"><Users/><span>Clienti</span></NavLink>
-    <NavLink to="/focus"><TimerReset/><span>Focus</span></NavLink>
-    <NavLink to="/calendario"><CalendarDays/><span>Calendario</span></NavLink>
-    <NavLink to="/altro"><MoreHorizontal/><span>Altro</span></NavLink>
+    <NavLink to="/" onClick={goTop}><Home/><span>Oggi</span></NavLink>
+    <NavLink to="/clienti" onClick={goTop}><Users/><span>Clienti</span></NavLink>
+    <NavLink to="/focus" onClick={goTop}><TimerReset/><span>Focus</span></NavLink>
+    <NavLink to="/calendario" onClick={goTop}><CalendarDays/><span>Calendario</span></NavLink>
+    <NavLink to="/altro" onClick={goTop}><MoreHorizontal/><span>Altro</span></NavLink>
   </nav>
 }
